5461bdb8e454cbc67e5c490aff47b64754c3a728
大可 <hnlq.sysu@gmail.com>
2022-05-16 10:52:23 +0800

    test: add normal case SayHello

--- tests/e2e/suite_example_test.go
+++ tests/e2e/suite_example_test.go
@@ -1,6 +1,9 @@
 package e2e
 
 import (
+	"context"
+
+	helloworldv1 "github.com/douyu/jupiter-layout/gen/api/go/helloworld/v1"
 	mocks "github.com/douyu/jupiter-layout/gen/mocks/grpc"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -13,9 +16,18 @@ var _ = Describe("exampleService", func() {
 	exampleService := CreateExampleService()
 	exampleService.ExampleGrpc = mockExampleGrpc
 
-	Context("List", func() {
+	Context("SayHello", func() {
 		It("normal case", func() {
 			Expect(exampleService).ShouldNot(BeNil())
+
+			res, err := exampleService.SayHello(context.Background(), &helloworldv1.SayHelloRequest{
+				Name: "bob",
+			})
+			Expect(err).Should(BeNil())
+
+			Expect(res).Should(Equal(&helloworldv1.SayHelloResponse{
+				Message: "hello wolrd",
+			}))
 		})
 	})
 })
