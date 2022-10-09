93a4b8d718293cb0cdec85b03422c9e3e6f70871
大可 <hnlq.sysu@gmail.com>
2022-10-09 04:21:36 +0000

    update

--- data/config/juno/init.sh
+++ data/config/juno/init.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-echo "starting juno-admin..."
+echo "init juno-admin..."
 /usr/local/bin/juno-admin --config=/root/juno/config/install.toml --install=true
 /usr/local/bin/juno-admin --config=/root/juno/config/install.toml --mock=true
-/usr/local/bin/juno-admin --config=/root/juno/config/test.toml
+
