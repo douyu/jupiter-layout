bf3e8f348baadf477df4a50b64fde24a0c05277f
大可 <hnlq.sysu@gmail.com>
2022-10-10 09:37:48 +0000

    feat: improve deployment workflow

--- deployment/data/config/juno/entry.sh
+++ deployment/data/config/juno/entry.sh
@@ -3,4 +3,5 @@
 echo "init juno-admin..."
 /usr/local/bin/juno-admin --config=/root/juno/config/install.toml --install=true
 /usr/local/bin/juno-admin --config=/root/juno/config/install.toml --mock=true
+/usr/local/bin/juno-admin --config=/root/juno/config/single-region-admin.toml --host=0.0.0.0
 
