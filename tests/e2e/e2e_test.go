80ece82e5474e0b5e02873e33d4c5d63b2b85dfe
大可 <hnlq.sysu@gmail.com>
2022-10-27 18:14:48 +0800

    update

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -2,14 +2,28 @@ package e2e
 
 import (
 	"testing"
+	"time"
 
+	"github.com/BurntSushi/toml"
+	"github.com/douyu/jupiter"
+	"github.com/douyu/jupiter-layout/internal/app/exampleserver/server"
+	"github.com/douyu/jupiter/pkg/conf"
+	"github.com/douyu/jupiter/pkg/conf/datasource/file"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
 func TestMockSuites(t *testing.T) {
-	stop := startServer()
-	defer stop()
+	// load config
+	conf.LoadFromDataSource(file.NewDataSource("../../config/exampleserver/local-live.toml", false), toml.Unmarshal)
+
+	app := jupiter.DefaultApp()
+	server.InitApp(app)
+	go app.Run()
+	defer app.Stop()
+
+	// wait for server start
+	time.Sleep(time.Second)
 
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "mock test cases")
