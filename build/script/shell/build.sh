aa4ae8dfe7586ee53e45b733aa25e563bf26ac4e
chory <chory@douyu.tv>
2023-02-08 10:24:50 +0800

    adjust

--- build/script/shell/build.sh
+++ build/script/shell/build.sh
@@ -6,6 +6,9 @@ basePath=$(dirname $(dirname $(dirname $(dirname $(readlink -f $0)))))
 echo -e "\nBuilding binaries in ${basePath}"
 declare -a bins=${@:2}
 
+# 开启gomod
+export GO111MODULE=on
+
 cd ${basePath}
 rm -rf ./bin/*
 for task in ${bins[@]}
@@ -14,16 +17,7 @@ do
     read pkgPath  <<< $(echo ${task} | awk -F ":" '{ print $1 }')
     read binName  <<< $(echo ${task} | awk -F ":" '{  print $2 }')
     echo pkgPath:${pkgPath}, binName:${binName}
+    echo ldflags:$1
     go build -ldflags "$1" -o ./bin/$(echo $(echo ${binName} -v | awk -F/ '{print $NF}') ${pkgPath})
     echo -e "\n"
 done
-
-if [ -d "${basePath}/cmd" ];then
-    for file in ${basePath}/cmd/*; do
-        echo building ${file}...
-        echo pkgPath:${file}, binName:$(basename  $file)
-        cd ${file}
-        go build -ldflags "$1" -o ${basePath}/bin/job/$(echo $(basename  $file))
-        echo -e "\n"
-    done
-fi
